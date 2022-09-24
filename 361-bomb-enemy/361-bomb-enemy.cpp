c2a4a365cf0f07d06cdc157417d619beba35a7f1
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2022-09-24 10:52:56 +0530

    Time: 192 ms (47.67%), Space: 20.7 MB (67.15%) - LeetHub

--- 361-bomb-enemy/361-bomb-enemy.cpp
+++ 361-bomb-enemy/361-bomb-enemy.cpp
@@ -7,58 +7,31 @@ public:
         int m = grid[0].size();
         int cnt = 0;
         
-        vector<vector<int>> right(n,vector<int>(m,0));
-        vector<vector<int>> left(n,vector<int>(m,0));
-        vector<vector<int>> down(n,vector<int>(m,0));
-        vector<vector<int>> up(n,vector<int>(m,0));
-        
-        //prefixing for right
-        for(int i=0 ; i<n ; ++i){
-            for(int j=m-1 ; j>=0 ; --j){
-                if(grid[i][j] == 'E') right[i][j] = 1;
-                if(grid[i][j] == 'W' || j == m-1) continue;
-                right[i][j] += right[i][j+1];
-            }
-        }
-        
-        //prefixing for left
-        for(int i=0 ; i<n ; ++i){
-            for(int j=0 ; j<m ; ++j){
-                if(grid[i][j] == 'E') left[i][j] = 1;
-                if(grid[i][j] == 'W' || j == 0) continue;
-                left[i][j] += left[i][j-1];
-            }
-        }
-        
-        //prefixing for up
-        for(int j=0 ; j<m ; ++j){
-            for(int i=0 ; i<n ; ++i){
-                if(grid[i][j] == 'E') up[i][j] = 1;
-                if(grid[i][j] == 'W' || i == 0) continue;
-                up[i][j] += up[i-1][j];
-            }
-        }
-        
-        
-        //prefixing for down
-        for(int j=0 ; j<m ; ++j){
-            for(int i=n-1 ; i>=0 ; --i){
-                if(grid[i][j] == 'E') down[i][j] = 1;
-                if(grid[i][j] == 'W' || i == n-1) continue;
-                down[i][j] += down[i+1][j];
-            }
-        }
-        
-        /*for(int i=0 ; i<n ; ++i){
-            for(int j=0 ; j<m ; ++j) cout<<down[i][j]<<" ";
-            cout<<endl;
-        }*/
+        vector<int> cols(m,0);
+        int row_enemies = 0;
         
         for(int i=0 ; i<n ; ++i){
             for(int j=0 ; j<m ; ++j){
-                if(grid[i][j] == '0') {
-                    cnt = max(cnt,left[i][j] + up[i][j] + down[i][j] + right[i][j]);
+                
+                //checking along each row now
+                if(j == 0 || grid[i][j-1] == 'W'){
+                    row_enemies = 0;
+                    for(int k=j ; k<m ; ++k){
+                        if(grid[i][k] == 'W') break;
+                        else if(grid[i][k] == 'E') row_enemies++;
+                    }
                 }
+                
+                //checking along the columns
+                if(i == 0 || grid[i-1][j] == 'W'){
+                    cols[j] = 0;
+                    for(int k=i ; k<n ; ++k){
+                        if(grid[k][j] == 'W') break;
+                        else if(grid[k][j] == 'E') cols[j]++; 
+                    }
+                }
+                
+                if(grid[i][j] == '0')cnt = max(cnt,row_enemies + cols[j]);
             }
         }
         return cnt;
