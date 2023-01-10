98f6e8475d48755ac958e8ba86b82fcd5f882ffe
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-01-10 17:54:01 +0530

    Time: 307 ms (43.50%), Space: 65.2 MB (17.26%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     
     bool static comp(vector<int> A, vector<int> B){
-        if(A[0] == B[0]) return A[1] < B[1];
-        return A[0] < B[0];
+        if(A[1] == B[1]) return A[1] < B[1];
+        return A[1] < B[1];
     }
 
     
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
         sort(pairs.begin(),pairs.end(),comp);
-        
-        vector<int> dp(n,1);
+        int len = 1;
+        int tail = pairs[0][1];
         
         for(int i=1 ; i<n ; ++i){
-            for(int j=0 ; j<i ; ++j){
-                if(pairs[i][0] > pairs[j][1] && dp[i] < (1 + dp[j])){
-                    dp[i] = 1 + dp[j];
-                }
+            if(pairs[i][0] > tail){
+                tail = pairs[i][1];
+                len += 1;
             }
         }
-        
-        int len = 1;
-        for(int i=0 ; i<n ; ++i) len = max(len,dp[i]);
+       
         return len;
     }
 };
