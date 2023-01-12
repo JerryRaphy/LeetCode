9da1c209f5f80b2d9a477a983fe304dd61f8e2d3
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-01-12 16:38:10 +0530

    Time: 98 ms (92.20%), Space: 24.2 MB (91.73%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    long long dp[100001][4][3];
-    long long solve(char prev,int i,int k,string &s){
-        if(k == 0) return 1;
-        if(i == s.size()) return 0;
-        if(dp[i][k][prev - '0'] != -1) return dp[i][k][prev - '0'];
-        
-        long long op1 = 0;
-        if(s[i] != prev) op1 = solve(s[i],i+1,k-1,s);
-        long long op2 = solve(prev,i+1,k,s);
-        return dp[i][k][prev - '0'] = op1 + op2;
-    }
     long long numberOfWays(string s) {
-        memset(dp,-1,sizeof dp);
-        return solve('2',0,3,s);
+        int n = s.length();
+        int cnt_ones = 0;
+        int cnt_zero = 0;
+        
+        for(int i=0 ; i<n ; ++i){
+            if(s[i] == '0') cnt_zero++;
+            else cnt_ones++;
+        }
+        
+        int curr_ones = 0;
+        int curr_zero = 0;
+        
+        long long cnt = 0;
+        
+        for(int i=0 ; i<n ; ++i){
+            if(s[i] == '1'){
+                cnt += (curr_zero * cnt_zero);
+                ++curr_ones;
+                --cnt_ones;
+            }
+            else{
+                cnt += (curr_ones * cnt_ones);
+                ++curr_zero;
+                --cnt_zero;
+            }
+        }
+        
+        return cnt;
     }
 };
