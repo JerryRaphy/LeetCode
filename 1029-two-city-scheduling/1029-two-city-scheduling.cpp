ac3bd973afb47218969a17150468aa949528736d
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-01-20 22:50:33 +0530

    Time: 8 ms (53.33%), Space: 8 MB (47.84%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-        
-    vector<vector<vector<int>>> memo;
-    
-    int helper(vector<vector<int>>& costs, int i, int c1, int c2)
-    {
-        
-        
-        if(c1 == 0 && c2 == 0) return 0;
-        
-        if(c1 < 0 || c2 < 0) return 1e9;
-            
-        if(memo[i][c1][c2] != -1) return memo[i][c1][c2];
-           
-        int ans1 = costs[i][0] + helper(costs, i+1, c1-1, c2);
-        int ans2 = costs[i][1] + helper(costs, i+1, c1, c2-1);
-        return  memo[i][c1][c2] = min(ans1, ans2);
-    }
-    
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
         int n = costs.size();
-        memo.resize(n,vector<vector<int>>(n,vector<int>(n,-1)));
-        int ans = helper(costs, 0, n/2, n/2);
-        return ans;
+        vector<pair<int,int>> v;
+        
+        for(int i=0 ; i<n ; ++i){
+            int diff = costs[i][1] - costs[i][0];
+            v.push_back({diff,i});
+        }
+        
+        sort(v.begin(),v.end());
+        
+        int res = 0;
+        for(int i=0 ; i<n/2 ; ++i){
+            int idx = v[i].second;
+            res += costs[idx][1];
+        }
+        
+        for(int i=n/2 ; i<n ; ++i){
+            int idx = v[i].second;
+            res += costs[idx][0];
+        }
+        
+        
+        return res;
+        
     }
 };
