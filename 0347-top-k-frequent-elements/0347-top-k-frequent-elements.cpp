fb7ec27850c68e5f8f7dce83bbeac30e642ba779
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-03-13 15:14:39 +0530

    Time: 14 ms (86.04%), Space: 15.7 MB (8.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,21 +4,24 @@ public:
         
         int n = nums.size();   
         vector<int> res;
-        
         map<int,int> mp;
-        priority_queue<pair<int,int>,vector<pair<int,int>>> pq;
         
-        for(int i=0 ; i<n ; ++i){
-            mp[nums[i]]++;
+        vector<vector<int>> freq(n+1);
+        for(int i=0 ; i<n ; ++i) mp[nums[i]]++;
+        
+        for(auto it : mp){
+            freq[it.second].push_back(it.first);
         }
         
-        for(auto it : mp) pq.push({it.second,it.first});
-        
-        while(k--){
-            res.push_back(pq.top().second);
-            pq.pop();
+        for(int i=n ; i>=1 ; --i){
+            for(auto element : freq[i]){
+                if(k == 0) return res;
+                res.push_back(element);
+                k -= 1;
+            }
         }
         
         return res;
+            
     }
 };
