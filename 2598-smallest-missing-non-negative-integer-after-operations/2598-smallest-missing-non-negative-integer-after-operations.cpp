9c99cfa4d7e26e15884d53e3eca0a6ee0da2c154
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-03-19 14:50:31 +0530

    Time: 857 ms (18.18%), Space: 122.2 MB (9.09%) - LeetHub

---
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        
+        int n = nums.size();
+        int res = 0;
+       
+        map<int,int> mp;
+        for(int i=0 ; i<n ; ++i){
+            int mod = (nums[i] % value + value) % value;
+            mp[mod] += 1;
+        }
+        
+        for(auto it : mp){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        
+        for(int i=0 ; i<=1000001 ; ++i){
+            int mod = i % value;
+            if(mp.find(mod) == mp.end()) return i;
+            else{
+                mp[mod] -= 1;
+                if(mp[mod] == 0) mp.erase(mod);
+            }
+        }
+        
+        return 0;
+    }   
+};
