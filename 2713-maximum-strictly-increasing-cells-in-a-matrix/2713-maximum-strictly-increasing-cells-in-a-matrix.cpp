3bef15040ec0768ad47949ecb690bbce90386d61
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-05-31 11:28:52 +0530

    Time: 1189 ms (47.29%), Space: 186.5 MB (72.37%) - LeetHub

---
+++ 2713-maximum-strictly-increasing-cells-in-a-matrix/2713-maximum-strictly-increasing-cells-in-a-matrix.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int maxIncreasingCells(vector<vector<int>>& mat) {
+        
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        vector<int> row(n,-1);
+        vector<int> col(m,-1);
+        
+        vector<vector<int>> grid(n,vector<int>(m,0));    
+        vector<vector<int>> v;
+        
+        for(int i=0 ; i<n ; ++i){
+            for(int j=0 ; j<m ; ++j){
+                v.push_back({mat[i][j],i,j});
+            }
+        }
+        
+        sort(v.rbegin(),v.rend());
+        int sz = n * m;
+        
+        
+        // for(int i=0 ; i<sz ; ++i) cout<<v[i][0]<<" "<<v[i][1]<<" "<<v[i][2]<<endl;
+        
+        int i = 0;
+        while(i < sz){
+            int j = i + 1;
+            while(j < sz && v[j][0] == v[i][0]) j += 1;
+            
+
+            for(int k=i ; k<j ; ++k){
+                int r = v[k][1];
+                int c = v[k][2];
+                int val = v[k][0];
+                
+                grid[r][c] = 1 + max(row[r],col[c]);
+            }
+            
+            for(int k=i ; k<j ; ++k){
+                int r = v[k][1];
+                int c = v[k][2];
+                
+                row[r] = max(row[r],grid[r][c]);
+                col[c] = max(col[c],grid[r][c]);
+            }
+        
+            
+            i = j;
+        }
+        
+        
+        int res = 0;
+        for(int i=0 ; i<n ; ++i){
+            for(int j=0 ; j<m ; ++j) res = max(res,grid[i][j]);
+        }
+        
+        return res + 1;
+    }
+};
