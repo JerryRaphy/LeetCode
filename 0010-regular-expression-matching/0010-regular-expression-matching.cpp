6956b18450ee8d88e36321c37b9ee29153c02cf0
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-06-03 08:38:12 +0530

    Time: 266 ms (8.68%), Space: 205.5 MB (5.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    
-    
-       bool isregularexpression(int i, int j, string A, string B, int n, int m, vector<vector<int>> &memo){
-        if(i >= n && j >= m) return true;
-        if(j >= m) return false;
+    bool isRegex(int i , int j , string A , string B , int n , int m){
+        if(i < 0 && j < 0) return true;
+        if(j < 0) return false; 
 
-        if(memo[i][j] != -1) return memo[i][j];
         bool match = false;
-        if(i < n && (A[i] == B[j] || B[j] == '.')) match = true;
+        if(B[j] == '*') return isRegex(i,j-1,A,B,n,m);
+        if(i>=0 && (A[i] == B[j] || B[j] == '.')) match = true;
+        if(j+1<m && B[j+1] == '*'){
+            return (isRegex(i,j-1,A,B,n,m) || (match && isRegex(i-1,j,A,B,n,m)));
+        }
 
-        if(j+1 < m && B[j+1] == '*')
-            if(isregularexpression(i,j+2,A,B,n,m,memo) || (match && isregularexpression(i+1,j,A,B,n,m,memo))) return memo[i][j] = true;
-           
-        if(match) return memo[i][j] = isregularexpression(i+1,j+1,A,B,n,m,memo);
-        return memo[i][j] = false;
+        if(match) return isRegex(i-1,j-1,A,B,n,m);
+        return false;
+        
     }
+
     
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        
-        vector<vector<int>> memo(n+1,vector<int>(m+1,-1));
-        return isregularexpression(0,0,s,p,n,m,memo);
+        return isRegex(n-1,m-1,s,p,n,m);
     }
 };
