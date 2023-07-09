2269bbfd470c5e70a855494efb14d0e6cf9522e2
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-07-09 11:24:17 +0530

    Time: 1158 ms (86.36%), Space: 264.6 MB (47.73%) - LeetHub

---
+++ 2768-number-of-black-blocks/2768-number-of-black-blocks.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> countBlackBlocks(int m, int n, vector<vector<int>>& coordinates) {
+        
+        map<long long,int> mp;
+        for(auto c : coordinates){
+            long long x = c[0];
+            long long y = c[1];
+            
+            if(c[0] < m - 1 && c[1] < n-1) mp[ (long long)c[1] * 1000000L +  c[0]]++;          
+            if(c[0] - 1 >= 0 && c[1] < n-1) mp[ (long long)c[1] * 1000000L+  (c[0] - 1)]++;
+            if(c[1] - 1 >= 0 && c[0] < m-1) mp[ (long long)(c[1] -1) * 1000000L +  c[0]]++;
+            if(c[0] - 1 >= 0 && c[1] - 1 >= 0) mp[ (long long)(c[1] -1) * 1000000L +  (c[0] - 1)]++;
+        }
+        
+        
+        // for(auto it : mp) cout<<it.first<<" "<<it.second<<endl;
+        
+        vector<long long> res(5,0);
+      
+        
+        for(auto it : mp) res[it.second] += 1; 
+        long long total = (long long)(m-1) * (long long)(n-1);
+        res[0] = total - (res[1] + res[2] + res[3] + res[4]);
+        
+        
+        return res;
+        
+    }
+};
