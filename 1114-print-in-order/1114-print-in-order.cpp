b5bae592803b408b83a0032724d780d9c75454d0
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-07-30 15:26:45 +0400

    Time: 9 ms (79.57%), Space: 7.2 MB (21.62%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,44 @@
+class Foo {
+public:
+    
+    std::mutex m;
+    std::condition_variable cv;
+    int turn;
+    
+    Foo() {
+        turn  = 0;
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        turn = 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+         
+        std::unique_lock<std::mutex> lock(m);
+        while(turn != 1){
+            cv.wait(lock);
+        }
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        turn = 2;
+        printSecond();
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        
+        std::unique_lock<std::mutex> lock(m);
+
+        while(turn != 2){
+            cv.wait(lock);
+        }
+        // printThird() outputs "third". Do not change or remove this line.
+        turn = 2;
+        printThird();
+    }
+};
