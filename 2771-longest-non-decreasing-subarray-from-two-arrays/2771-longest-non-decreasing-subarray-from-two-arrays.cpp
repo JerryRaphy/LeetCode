73e993354109a7331edbdf14f646ae47cc63c8f8
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-08-22 01:06:23 +0530

    Time: 226 ms (92.14%), Space: 188.8 MB (36.16%) - LeetHub

--- 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
+    
+        
+   
+        
+        
+        
+        
+        
+    
+    
     int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
         
         int n = nums1.size();
-        vector<int> dp1(n,1);
-        vector<int> dp2(n,1);
+        vector<vector<int>> dp(n,vector<int>(2,1));
         
-        int len = 1;
-        
-        for(int i=n-2 ; i>=0 ; --i){
-            if(nums1[i] <= nums1[i+1]) dp1[i] = max(dp1[i],1 + dp1[i+1]);
-            if(nums1[i] <= nums2[i+1]) dp1[i] = max(dp1[i],1 + dp2[i+1]);
+        for(int idx=n-2 ; idx>=0 ; --idx){
             
-            if(nums2[i] <= nums2[i+1]) dp2[i] = max(dp2[i],1 + dp2[i+1]);
-            if(nums2[i] <= nums1[i+1]) dp2[i] = max(dp2[i],1 + dp1[i+1]);
-            
-            len = max(len,max(dp1[i],dp2[i]));
+            if(nums1[idx] <= nums1[idx+1] && nums1[idx] <= nums2[idx+1]) {
+            dp[idx][0] += max(dp[idx+1][0],dp[idx+1][1]);
+            }
+            else if(nums1[idx] <= nums1[idx+1]) dp[idx][0] += dp[idx+1][0];
+            else if(nums1[idx] <= nums2[idx+1]) dp[idx][0] += dp[idx+1][1];
+
+            if(nums2[idx] <= nums1[idx+1] && nums2[idx] <= nums2[idx+1]) {
+                dp[idx][1] += max(dp[idx+1][0],dp[idx+1][1]);
+            }
+            else if(nums2[idx] <= nums1[idx+1]) dp[idx][1] += dp[idx+1][0];
+            else if(nums2[idx] <= nums2[idx+1]) dp[idx][1] += dp[idx+1][1];
         }
-       
-        return len;
+        
+        int ans = 1;
+        for(int i=0 ; i<n ; ++i) ans = max(ans,max(dp[i][0],dp[i][1]));
+        
+        return ans;
     }
 };
