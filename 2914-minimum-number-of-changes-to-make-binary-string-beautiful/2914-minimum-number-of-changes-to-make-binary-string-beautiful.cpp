2a0ae65f0767578a182731b71eaf0e7cd587c807
Jerald Kannath <65911778+JerryRaphy@users.noreply.github.com>
2023-10-29 12:05:43 +0530

    Time: 14 ms (57.14%), Space: 10.3 MB (85.71%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        
+        int n = s.length();
+        int res = 0;
+        
+        for(int i=1 ; i<n ; i+=2){
+            if(s[i] != s[i-1]) res += 1;
+        }
+        
+        return res; 
+    }
+};
